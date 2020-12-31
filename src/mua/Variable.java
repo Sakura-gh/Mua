87098121886a47c883ea251f0d1d7021ad5da1d2
Sakura-gh <Sakura.gehao@gmail.com>
2020-12-31 19:26:03 +0800

    add save and clear variable feature

--- src/mua/Variable.java
+++ src/mua/Variable.java
@@ -1,6 +1,13 @@
 package mua;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Variable {
@@ -8,6 +15,11 @@ public class Variable {
 
     public Variable() {
         variablePool = new HashMap<>();
+        loadConstantVariable();
+    }
+
+    private void loadConstantVariable() {
+        variablePool.put("pi", String.valueOf(Math.PI));
     }
 
     public String getValue(String key) {
@@ -31,4 +43,30 @@ public class Variable {
     public String exsitKey(String key) {
         return String.valueOf(variablePool.containsKey(key));
     }
+
+    public void clearVariable() {
+        variablePool.clear();
+    }
+
+    public String getVarible() {
+        String result = "[";
+        for (String key : variablePool.keySet()) {
+            result += key + " ";
+        }
+        result = result.trim() + "]";
+        return result;
+    }
+
+    public void saveVariable(String fileName) {
+        try {
+            BufferedWriter out = new BufferedWriter(new FileWriter(fileName));
+            for (Map.Entry<String, String> variable : variablePool.entrySet()) {
+                out.write("make \"" + variable.getKey() + " " + variable.getValue() + "\n");
+            }
+            out.close();
+        } catch (IOException e) {
+            System.out.println("save error!");
+        }
+    }
+
 }
